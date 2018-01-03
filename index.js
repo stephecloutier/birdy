48bee548f0be84a04cb258b71cbceb9660eea5cf
stephecloutier <stephecloutier@gmail.com>
2018-01-03 19:13:50 +0100

    Added thunk middleware, used it in store configuration, created store variable in index

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import store from './src/store';
+import configureStore from './src/store/configureStore';
 
+const store = configureStore();
 
 export default class Birdy extends Component {
     render() {
