31898ee6c7c89581a07e84ed6584e1eff355b946
stephecloutier <stephecloutier@gmail.com>
2018-01-28 15:36:15 +0100

    Added firebase settings

---
+++ settings.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+    apiKey: "AIzaSyD_VNuwA0TZEOiX--YKwQt_i-u1__Ark-U",
+    authDomain: "birdy-aa3c7.firebaseapp.com",
+    databaseURL: "https://birdy-aa3c7.firebaseio.com",
+    projectId: "birdy-aa3c7",
+    storageBucket: "birdy-aa3c7.appspot.com",
+    messagingSenderId: "831149396407"
+  };
